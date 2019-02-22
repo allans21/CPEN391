c594ce553e66d1b4ac3cba73a3013788a74aea7f
Aidan Rosswood <aidoraide@gmail.com>
2019-02-22 13:58:20 -0800

    tweaks

--- server/app.py
+++ server/app.py
@@ -55,50 +55,16 @@ def serveimg(fname):
     return response
 
 
-@app.route('/upload_img', methods=['POST'])
-def upload_img():
-    print("request.data", request.data)
-    print("request.form", request.form)
-    if 'img_id' not in request.form:
-        return 'Failure. Could not find media.', 400
-    img_id = str(int(request.form['img_id'].strip('&')))
-    chunk_num = 0
-    fname = 'img_store/' + img_id  + '_0.b64'
-    b64_img = []
-    while os.path.exists(fname):
-        b64_img.append(open(fname).read())
-        chunk_num += 1
-        os.remove(fname)
-        fname = 'img_store/' + img_id + '_' + str(chunk_num) + '.b64'
-    
+@app.route('/signin', methods=['POST'])
+def signin():
+    # TODO get user email
+    print(request.form)
+    email, userpass = request.form.get('email'), request.form.get('userpass')
+    if not email or not userpass:
+        return "Missing email or password", 400
 
-    b64_img = ''.join(b64_img)
-    if len(b64_img) % 4 != 0:
-        b64_img = b64_img + ('=' * (4-(len(b64_img)%4)))
-    print(b64_img)
-    img_bytes = b64decode(b64_img)
-    img = Image.open(BytesIO(img_bytes))
-    img.save('img_store/' + img_id + '.jpg')
-    return fname
+    return render_template('SignIn.html', username=email)
 
-@app.route('/upload_chunk', methods=['POST'])
-def upload_chunk():
-    if 'img_id' not in request.form:
-        return 'No img_id', 400
-    if 'chunk' not in request.form:
-        return 'No chunk', 400
-    if 'chunk_num' not in request.form:
-        return 'No chunk_num', 400
-     
-    chunk_num = str(int(request.form['chunk_num'].strip('&')))
-    img_id = str(int(request.form['img_id'].strip('&')))
-    chunk = request.form['chunk'].replace(' ', '+').strip('&').strip() # Flask decodes the +'s to space. We need the +'s as they are for B64
-    print('%s #%s == %s' % (img_id, chunk_num, chunk))
-    fname = 'img_store/' + img_id + '_' + chunk_num + '.b64'
-    with open(fname, 'a+') as f:
-        f.write(chunk)
-    
-    return 'OK', 200
 
 @app.route('/user', methods=['GET'])
 def get_user():
