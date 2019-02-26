bdeb99d9e43daaabe736db1b64ce46fd8f32c4db
Aidan <aidoraide@gmail.com>
2019-02-26 04:36:09 +0000

    hehehe kyle

--- server/db/create_db.py
+++ server/db/create_db.py
@@ -1,7 +1,10 @@
 import utils
 
 db_conn = utils.get_connection()
+db_conn.autocommit = True
 print("starting db script")
 curs = db_conn.cursor()
 query = open('create_db.sql').read()
 curs.execute(query)
+
+
